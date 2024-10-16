3dfaf206fe463bced58efc36c49b3d1009d03f6b
Kei-K23 <keiksl2301@gmail.com>
2024-10-16 18:10:02 +0630

    Implement request body parsing method in Request class

--- src/http/request.ts
+++ src/http/request.ts
@@ -10,4 +10,27 @@ export class Request {
   get method(): string | undefined {
     return this.req.method;
   }
+
+  public async json<T = unknown>(): Promise<T> {
+    return new Promise((resolve, reject) => {
+      let body: Uint8Array[] = [];
+
+      this.req
+        .on("error", (err) => {
+          reject(err);
+        })
+        .on("data", (chunk) => {
+          body.push(chunk);
+        })
+        .on("end", () => {
+          try {
+            const parsedBody = JSON.parse(Buffer.concat(body).toString());
+
+            resolve(parsedBody as T);
+          } catch {
+            reject(new Error("Invalid JSON"));
+          }
+        });
+    });
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
 import { MicroServer } from "./core/server";
+import { Request } from "./http/request";
 import { Router } from "./router/router";
 
 const router = new Router();
 const server = new MicroServer(router, { port: 8080 });
 
+interface User {
+  name: string;
+  age: number;
+}
+
 router.get("/", (req, res) => {
   res.json({ message: "hello world again" });
 });
 
-router.get("/about", (req, res) => {
+router.get("/about", (req: Request, res) => {
   res.json({ message: "This is about" });
+});
+
+router.post("/", async (req: Request, res) => {
+  const body = await req.json<User>();
+  res.json({ message: "This is about", name: body.name, age: body.age });
 });
 
 server.listen();
